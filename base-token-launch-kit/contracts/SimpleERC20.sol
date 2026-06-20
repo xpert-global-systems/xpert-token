a28dad76ce09a01785b4db568ad52560c18c3175
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-06-20 05:05:13 +0100

    Update SimpleERC20.sol
    Signed-off-by: LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>

--- base-token-launch-kit/contracts/SimpleERC20.sol
+++ base-token-launch-kit/contracts/SimpleERC20.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SimpleERC20 is ERC20 {
+contract SimpleERC20 is ERC20, Ownable {
+
     constructor(
         string memory name_,
         string memory symbol_,
         uint256 initialSupply_
-    ) ERC20(name_, symbol_) {
+    ) ERC20(name_, symbol_) Ownable(msg.sender) {
         _mint(msg.sender, initialSupply_ * 10 ** decimals());
+    }
+
+    // 🔥 MINT FUNCTION
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
